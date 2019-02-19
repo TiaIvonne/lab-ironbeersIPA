cb9d8cda4257ebbf6d52ded552274145fe007738
Ivonne Yanez Mendoza <ivonne.wftm@gmail.com>
2019-02-19 16:54:14 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,56 @@
 
 const express = require('express');
-const hbs     = require('hbs');
+const hbs     = require('express-handlebars');
 const app     = express();
 const path    = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-const punkAPI = new PunkAPIWrapper();
+
+
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
+
+app.engine( 'hbs', hbs( { 
+  extname: 'hbs', 
+  defaultLayout: 'main', 
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials/'
+} ) );
 
 
-
-app.get('/', (req, res, next) => {
+//call index
+app.get('/', (req, res) => {
   res.render('index');
 });
 
+//call an object with beers details
+const beersObject = require('./beersObject.json')
+
+app.get('/beers',(req, res, next) => {
+  res.render('beers', {beers: beersObject});
+})
+
+//call random beers
+var getRandomBeer = () => {
+  var i = Math.floor(Math.random() * beersObject.length);
+  return i;
+};
+
+var randomIndex = getRandomBeer();
+var randomBeerObject = beersObject[randomIndex];
+
+app.get('/randomBeer', (req, res) => {
+  // console.log(randomBeerObject)
+  res.render('randomBeer', {randomBeerObject: randomBeerObject})
+});
+
+//call footer
+app.get('/footer', (req, res) => {
+  res.render('footer');
+});
 
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("listening")
+})
